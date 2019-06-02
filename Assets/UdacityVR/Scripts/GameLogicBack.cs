d3a627446f5e4e4805886c3fae981db07a338444
Raisa_Bacalova <lemuria98@gmail.com>
2019-06-02 12:54:05 -0400

    Initial commit

---
+++ Assets/UdacityVR/Scripts/GameLogicBack.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameLogicBack : MonoBehaviour {
+
+    public GameObject closerUI, startUI;
+
+    public void ToggleUI()
+    {
+        closerUI.SetActive(!closerUI.activeSelf);
+        startUI.SetActive(!startUI.activeSelf);
+    }
+}
---
+++ Assets/UdacityVR/Scripts/GameLogicCloser.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameLogicCloser : MonoBehaviour
+
+
+	{
+	public GameObject rulesUI, closerUI;
+
+    public void ToggleUI()
+    {
+        rulesUI.SetActive(!rulesUI.activeSelf);
+        closerUI.SetActive(!closerUI.activeSelf);
+    }
+}
